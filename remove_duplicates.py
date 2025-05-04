efd11563d61cbd08226a6c0a95ffa74223dd8179
SAMBARAN <sambarandas0@gmail.com>
2025-05-04 09:32:56 +0530

    remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,3 +1,8 @@
-class Solution:
-    def removeduplicates(self, nums):
-        
+class Solution(object):
+    def removeDuplicates(self, nums):
+        l = 1
+        for r in range(1,len(nums)):
+            if nums[r]  != nums[r-1]:
+                nums[l] = nums[r]
+                l+=1
+        return l
